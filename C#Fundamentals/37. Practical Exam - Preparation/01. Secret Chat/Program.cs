db9b9663748e13cae61e2b06e1f402214a9b8163
Boris Eftimov <b_eftimov@yahoo.com>
2021-11-21 20:54:43 +0200

    more

---
+++ C#Fundamentals/37. Practical Exam - Preparation/01. Secret Chat/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _01._Secret_Chat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string msg = Console.ReadLine();
+            string input = Console.ReadLine();
+            StringBuilder message = new StringBuilder();
+            message.Append(msg);
+            while (input != "Reveal")
+            {
+                string[] cmd = input.Split(":|:");
+                if (cmd[0] == "InsertSpace")
+                {
+                    int index = int.Parse(cmd[1]);
+                    message.Insert(index, " ");
+                    
+                }
+                else if (cmd[0] == "Reverse")
+                {
+                    string substring = cmd[1];
+                    if (message.ToString().Contains(substring))
+                    {
+                        var regex = new Regex(Regex.Escape(substring));             //
+                        var newMessage = regex.Replace(message.ToString(), "", 1);  //replace only the first instance of substring in the message
+                        message.Clear();
+                        message.Append(newMessage);
+                        message.Append(Reverse(substring));
+                    }
+                    else
+                    {
+                        Console.WriteLine("error");
+                        input = Console.ReadLine();
+                        continue;
+                    }
+                    
+                }
+                else if (cmd[0] == "ChangeAll")
+                {
+                    string occurance = cmd[1];
+                    string replacement = cmd[2];
+                    message.Replace(occurance, replacement);
+                }
+                Console.WriteLine(message);
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"You have a new text message: {message}");
+        }
+        public static string Reverse(string s)  //reverse a string
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
---
+++ C#Fundamentals/37. Practical Exam - Preparation/02. Mirror Words/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02._Mirror_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string regexKey = @"(?<sep>[@#])(?<word>[A-Za-z]{3,})(\k<sep>)(\k<sep>)(?<mirror>[A-Za-z]{3,})(\k<sep>)";
+            MatchCollection pairs = Regex.Matches(input, regexKey);
+            if (pairs.Count>0)
+            {
+                Console.WriteLine($"{pairs.Count} word pairs found!");
+            }
+            else
+            {
+                Console.WriteLine("No word pairs found!");
+                Console.WriteLine("No mirror words!");
+                return;
+            }
+            
+            Dictionary<string, string> wordMirror = new Dictionary<string, string>();
+            foreach (Match pair in pairs)
+            {
+                wordMirror.Add(pair.Groups["word"].Value, Reverse(pair.Groups["mirror"].Value));
+            }
+            foreach (var pair in wordMirror)
+            {
+                if (pair.Key!=pair.Value)
+                {
+                    wordMirror.Remove(pair.Key);
+                }
+            }
+            if (wordMirror.Count==0)
+            {
+                Console.WriteLine("No mirror words!");
+            }
+            else
+            {
+                Console.WriteLine("The mirror words are:");
+                Console.WriteLine(string.Join(", ", wordMirror.Select(x => x.Key + " <=> " + Reverse(x.Value))));
+                
+            }
+
+        }
+        public static string Reverse(string s)  //reverse a string
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
