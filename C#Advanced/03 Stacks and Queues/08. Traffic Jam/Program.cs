ec53c460ace2f960f5ae2a862d3e30730ac533ea
Boris Eftimov <b_eftimov@yahoo.com>
2022-01-12 23:52:51 +0200

    Stacks and Queues

--- C#Advanced/03 Stacks and Queues/08. Traffic Jam/Program.cs
+++ C#Advanced/03 Stacks and Queues/08. Traffic Jam/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _08._Traffic_Jam
 {
@@ -6,7 +7,32 @@ namespace _08._Traffic_Jam
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            Queue<string> traffic = new Queue<string>();
+            int passedCars = 0;
+            while (input!="end")
+            {
+                if (input!="green")
+                {
+                    traffic.Enqueue(input);
+                }
+                else
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        if (traffic.Count>0)
+                        {
+                            Console.WriteLine($"{traffic.Dequeue()} passed!");
+                            passedCars++;
+                        }
+                        
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"{passedCars} cars passed the crossroads.");
         }
     }
 }
