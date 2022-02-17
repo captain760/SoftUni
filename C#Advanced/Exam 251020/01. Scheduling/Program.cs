41b768975128373d6f3a14541f62bb5997fd7a69
Boris Eftimov <b_eftimov@yahoo.com>
2022-02-17 12:04:02 +0200

    exam preps

---
+++ C#Advanced/Exam 251020/01. Scheduling/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Scheduling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] taskElements = Console.ReadLine()
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+            Stack<int> tasks = new Stack<int>(taskElements);
+            int[] threadsElements = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+            Queue<int> threads = new Queue<int>(threadsElements);
+            int taskToKill = int.Parse(Console.ReadLine());
+            bool taskKilled = false;
+            while (tasks.Count>0 && threads.Count>0)
+            {
+                if (tasks.Peek() == taskToKill)
+                {
+                    taskKilled = true;
+                    break;
+                }
+                if (threads.Peek()>=tasks.Peek())
+                {
+                    threads.Dequeue();
+                    tasks.Pop();
+                }
+                else
+                {
+                    threads.Dequeue();
+                }
+            }
+            if (taskKilled)
+            {
+                Console.WriteLine($"Thread with value {threads.Peek()} killed task {taskToKill}");
+            }
+            Console.WriteLine(string.Join(" ",threads));
+        }
+    }
+}
---
+++ C#Advanced/Exam 251020/02. Garden/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Garden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+            int n = size[0];
+            int m = size[1];
+            int[,] plot = new int[n, m];
+            string input = Console.ReadLine();
+            while (input!="Bloom Bloom Plow")
+            {
+                int[] cmd = input
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                int row = cmd[0];
+                int col = cmd[1];
+                if (!IsValid(plot,row,col))
+                {
+                    Console.WriteLine("Invalid coordinates.");
+                    input = Console.ReadLine();
+                    continue;
+                }
+                for (int i = 0; i < n; i++)
+                {
+                    plot[i, col]++;
+                }
+                for (int j = 0; j < m; j++)
+                {
+                    plot[row, j]++;
+                }
+                plot[row, col]--;
+                input = Console.ReadLine();
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(plot[i,j]+" ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static bool IsValid(int[,] plot, int row, int col)
+        {
+            if (row>=0 && col>=0 &&row<plot.GetLength(0) && col<plot.GetLength(1))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
