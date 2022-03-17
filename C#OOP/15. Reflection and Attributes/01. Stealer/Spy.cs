8893b4c53da5ef99b3b291313cb72428bf0f8572
Boris Eftimov <b_eftimov@yahoo.com>
2022-03-18 00:24:30 +0200

    Reflection and Attributes Lab

---
+++ C#OOP/15. Reflection and Attributes/01. Stealer/Spy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string className, params string[] fieldsArray)
+        {
+            Type classType = Type.GetType(className);
+            FieldInfo[] classFields = classType.GetFields((BindingFlags)60);
+            StringBuilder sb = new StringBuilder();
+            Object classInstance = Activator.CreateInstance(classType, new object[] { });
+            sb.AppendLine($"Class under investigation: {className}");
+            foreach (FieldInfo field in classFields.Where(x => fieldsArray.Contains(x.Name)))
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+                
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ C#OOP/15. Reflection and Attributes/High Quality Mistakes/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Stealer
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
