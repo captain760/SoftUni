1b7accb4f1050b6e456e5ddd144ad62333e967bb
Boris Eftimov <b_eftimov@yahoo.com>
2022-03-17 09:46:47 +0200

    SOLID lab exercises

---
+++ C#OOP/12.SOLID Lab/Lab-Skeleton/P01.Stream_Progress/IFile.cs
@@ -0,0 +1,11 @@
+﻿namespace P01.Stream_Progress
+{
+    public interface IFile
+    {
+        
+
+        public int Length { get; set; }
+
+        public int BytesSent { get; set; }
+    }
+}
---
+++ C#OOP/12.SOLID Lab/Lab-Skeleton/P01.Stream_Progress/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace P01.Stream_Progress
+{
+    public class Program
+    {
+        static void Main()
+        {
+            IFile file = new File("bobo", 100, 25);
+            StreamProgressInfo progress = new StreamProgressInfo(file);
+            Console.WriteLine(progress.CalculateCurrentPercent());
+            IFile audio = new Music("bobo", "First", 50, 25);
+            StreamProgressInfo progressAudio = new StreamProgressInfo(audio);
+            Console.WriteLine(progressAudio.CalculateCurrentPercent());
+            IFile video = new Video("toto", 200, 135);
+            StreamProgressInfo progressVideo = new StreamProgressInfo(video);
+            Console.WriteLine(progressVideo.CalculateCurrentPercent());
+        }
+    }
+}
