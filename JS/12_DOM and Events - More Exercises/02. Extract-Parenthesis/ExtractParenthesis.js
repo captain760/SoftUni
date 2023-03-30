61ba33570f22497ce9f8dc4ff2ea42f6af4d749f
Boris Eftimov <b_eftimov@yahoo.com>
2023-03-30 11:57:26 +0300

    More exercises done...

--- JS/12_DOM and Events - More Exercises/02. Extract-Parenthesis/ExtractParenthesis.js
+++ JS/12_DOM and Events - More Exercises/02. Extract-Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,23 @@
 function extract(content) {
 
+// *** with RegEx***
+// const strObj= document.getElementById(content);
+// let final = [];
+// let result = strObj.textContent.split(/[()]/);
+// for (let i = 1; i < result.length; i+=2) {
+//   final.push(result[i]);    
+// }
+// return final.join('; ');
+
+const str= document.getElementById(content).textContent;
+let final = [];
+let openBrArr = str.split('(');
+for (const firstSplit of openBrArr) {
+    if (firstSplit.includes(')')){
+        let closBrIndex = firstSplit.indexOf(')');
+        let text = firstSplit.substring(0,closBrIndex);
+        final.push(text);
+    }
+}
+return final.join('; ');
 }
